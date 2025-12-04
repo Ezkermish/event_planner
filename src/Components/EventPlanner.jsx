5906c4f5ce9b73f52f6a7ae4221edb1b6681abb3
Ezkermish <carlosa.quiroz@gmail.com>
2025-12-03 22:09:32 -0500

    Primeras modificaciones

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -5,8 +5,13 @@ const EventPlanner = () => {
     return (
         <div className="event-planner-container">
             <header>
-                <h1>Welcome to Event Planner</h1>
+                <h1>Bienvenido a Event Planner</h1>
             </header>
+            <section className="description"></section>
+            <section className="event_categories"></section>
+            <section className="features"></section>
+            <section className="testimonials"></section>
+            <section className="contact"></section>
         </div>
     );
 };
